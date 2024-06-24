7b6e8a1e06d66116e60f2ee8bf4ac2ed6c8a4f73
Andrey-Shushkanov <shushkanovandrey@mail.ru>
2024-06-24 23:54:22 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import devide as fake_dev
+from true_math import devide as true_dev
+
+result1 = fake_dev(432, 9)
+result2 = fake_dev(20, 0)
+result3 = true_dev(1232, 8)
+result4 = true_dev(44, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
